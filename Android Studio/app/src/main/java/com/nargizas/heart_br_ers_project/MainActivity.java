45bacbc6b86a6ba0660a52ca13f7de1f7e1fd6b5
kekland <kk.erzhan@gmail.com>
2018-01-22 20:05:30 +0600

    Fixed ZIP

---
+++ Android Studio/app/src/main/java/com/nargizas/heart_br_ers_project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nargizas.heart_br_ers_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
