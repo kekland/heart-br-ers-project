70df9668d9e1aab0bc6095425324bb7bc8cb15c0
Lyubov Dudchenko <lyuba@MacBook-Pro-Lyubov.local>
2018-02-03 17:25:17 +0600

    Added Select Bar concept

---
+++ Concepts/SelectBar/Assets/scripts/createnew.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createnew : MonoBehaviour {
+	public Vector3 pos;
+	void Update(){
+		if (Input.GetMouseButtonDown (0)) {
+			//GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			//cube.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0); 
+			//Debug.Log(Input.mousePosition);
+			//Debug.Log(Camera.main.Sc ToWorldPoint(Input.mousePosition));
+		}
+	}
+	/*void createCube(){
+		GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		cube.transform.position = Input.mousePosition; 
+	}
+	void createSphere(){
+		GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		sphere.transform.position = pos; 
+	}*/
+}
---
+++ Concepts/SelectBar/Assets/scripts/drag2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drag2 : MonoBehaviour {
+	private Vector3 screenPoint; 
+	// Use this for initialization
+	void OnMouseDown(){
+		screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+	}
+
+	void OnMouseDrag(){
+		Vector3 currentScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); 
+		Vector3 currentPos = Camera.main.ScreenToWorldPoint (currentScreenPoint); 
+		transform.position = currentPos; 
+	}
+}
